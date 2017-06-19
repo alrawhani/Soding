4cbc8db8ae3feed6a5cdcb430c3dcf7333d347cb
alrawhani <a.arawhani@gmail.com>
2017-06-20 06:54:33 +0800

    Add files via upload

---
+++ SodingTasksManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SodingTasksManager.Startup))]
+namespace SodingTasksManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
